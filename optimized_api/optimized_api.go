88be2a34aec45928acace4d10692cb63673ae152
nireo <eemil.lehtikangas1@gmail.com>
2020-09-22 20:59:13 +0300

    added register route and setup the database

--- optimized_api/optimized_api.go
+++ optimized_api/optimized_api.go
@@ -3,11 +3,15 @@ package optimized_api
 import (
 	"fmt"
 	"log"
+	"net/http"
+	"os"
 	"path/filepath"
 
 	"github.com/buaazp/fasthttprouter"
+	"github.com/jinzhu/gorm"
 	"github.com/nireo/upfi/lib"
 	"github.com/nireo/upfi/models"
+	"github.com/nireo/upfi/server"
 	"github.com/valyala/fasthttp"
 )
 
@@ -20,6 +24,53 @@ func NotFoundHandler(ctx *fasthttp.RequestCtx) {
 	ctx.Response.SetStatusCode(fasthttp.StatusNotFound)
 }
 
+func Register(ctx *fasthttp.RequestCtx) {
+	form, err := ctx.MultipartForm()
+	if err != nil {
+		ctx.Error("Content type needs to be multipart", fasthttp.StatusBadRequest)
+		return
+	}
+
+	if len(form.Value["username"]) == 0 || len(form.Value["password"][0]) == 0 {
+		ctx.Error("Both username and password fields must be added", fasthttp.StatusNotFound)
+		return
+	}
+
+	username := form.Value["username"][0]
+	password := form.Value["password"][0]
+
+	// check for conflicting users
+	_, err = models.FindOneUser(&models.User{Username: username})
+	if err == nil {
+		ctx.Error("User already exists with that username", fasthttp.StatusConflict)
+		return
+	}
+
+	hash, err := server.HashPassword(password)
+	if err != nil {
+		ctx.Error("Internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	newUser := models.User{
+		Username: username,
+		Password: hash,
+		UUID:     lib.GenerateUUID(),
+	}
+
+	// create a directory which stores all of the user's files
+	err = os.Mkdir("./files/"+newUser.UUID, os.ModePerm)
+	if err != nil {
+		ctx.Error("Failed user directory creation", fasthttp.StatusInternalServerError)
+		return
+	}
+
+	db := lib.GetDatabase()
+	db.NewRecord(newUser)
+	db.Create(&newUser)
+	ctx.Response.SetStatusCode(fasthttp.StatusOK)
+}
+
 func UploadFile(ctx *fasthttp.RequestCtx) {
 	// get file
 	header, err := ctx.FormFile("file")
@@ -30,7 +81,7 @@ func UploadFile(ctx *fasthttp.RequestCtx) {
 
 	form, err := ctx.MultipartForm()
 	if err != nil {
-		ctx.Error("Content type need to multipart", fasthttp.StatusBadRequest)
+		ctx.Error("Content type needs to be multipart", fasthttp.StatusBadRequest)
 		return
 	}
 
@@ -64,8 +115,21 @@ func UploadFile(ctx *fasthttp.RequestCtx) {
 
 func SetupOptimizedApi() {
 	router := fasthttprouter.New()
-	router.POST("/upload", UploadFile)
 
+	// Load database
+	db, err := gorm.Open("postgres", "host=localhost port=5432 user=postgres dbname=upfi sslmode=disable")
+	if err != nil {
+		panic(err)
+	}
+	models.MigrateModels(db)
+	defer db.Close()
+	lib.SetDatabase(db)
+
+	// setup routes
+	router.POST("/upload", UploadFile)
+	router.POST("/register", Register)
+
+	// start the http server
 	if err := fasthttp.ListenAndServe("localhost:8080", router.Handler); err != nil {
 		log.Fatalf("Error in ListenAndServe %s", err)
 	}
