2acaf92f9b4842b8ed898a80f77d9f5a715dee20
nireo <eemil.lehtikangas1@gmail.com>
2020-09-24 19:58:28 +0300

    added rest of the user routes

--- optimized_api/optimized_api.go
+++ optimized_api/optimized_api.go
@@ -27,18 +27,16 @@ func SetupOptimizedApi() {
 	router.POST("/upload", middleware.CheckAuthentication(UploadFile))
 	router.POST("/register", Register)
 	router.POST("/login", Login)
-
 	router.GET("/login", ServeLoginPage)
 	router.GET("/register", ServeRegisterPage)
-
 	router.GET("/file/:file", middleware.CheckAuthentication(GetSingleFile))
 	router.GET("/files", middleware.CheckAuthentication(GetUserFiles))
-
 	router.PATCH("/file/:file", middleware.CheckAuthentication(UpdateFile))
 	router.DELETE("/file/:file", middleware.CheckAuthentication(DeleteFile))
-
 	router.GET("/settings", middleware.CheckAuthentication(ServeSettingsPage))
 	router.POST("/settings", middleware.CheckAuthentication(HandleSettingChange))
+	router.DELETE("remove", middleware.CheckAuthentication(DeleteUser))
+	router.PATCH("/password", middleware.CheckAuthentication(UpdatePassword))
 
 	// start the http server
 	if err := fasthttp.ListenAndServe("localhost:8080", router.Handler); err != nil {
--- optimized_api/user.go
+++ optimized_api/user.go
@@ -1,10 +1,13 @@
 package optimized_api
 
 import (
+	"fmt"
+	"os"
 	"text/template"
 
 	"github.com/nireo/booru/lib"
 	"github.com/nireo/upfi/models"
+	"github.com/nireo/upfi/server"
 	"github.com/valyala/fasthttp"
 )
 
@@ -52,4 +55,66 @@ func HandleSettingChange(ctx *fasthttp.RequestCtx) {
 	user.Username = newUsername
 
 	ctx.Response.Header.SetStatusCode(fasthttp.StatusNoContent)
+}
+
+func DeleteUser(ctx *fasthttp.RequestCtx) {
+	username := string(ctx.Request.Header.Peek("username"))
+	db := lib.GetDatabase()
+
+	var user models.User
+	if err := db.Where(&models.User{Username: username}).First(&user).Error; err != nil {
+		ctx.Error(fasthttp.StatusMessage(fasthttp.StatusNotFound), fasthttp.StatusNotFound)
+		return
+	}
+
+	// remove the user files
+	if err := os.Remove(fmt.Sprintf("./files/%s", user.UUID)); err != nil {
+		ctx.Error(fasthttp.StatusMessage(fasthttp.StatusInternalServerError), fasthttp.StatusInternalServerError)
+		return
+	}
+
+	db.Delete(&user)
+
+	// remove the tokens
+	ctx.Request.Header.DelAllCookies()
+	ctx.Response.Header.SetStatusCode(fasthttp.StatusNoContent)
+	ctx.Write([]byte("Successfully removed all your files!"))
+}
+
+func UpdatePassword(ctx *fasthttp.RequestCtx) {
+	db := lib.GetDatabase()
+	username := string(ctx.Request.Header.Peek("username"))
+
+	var user models.User
+	if err := db.Where(&models.User{Username: username}).First(&user).Error; err != nil {
+		ctx.Error(fasthttp.StatusMessage(fasthttp.StatusNotFound), fasthttp.StatusNotFound)
+		return
+	}
+
+	form, err := ctx.MultipartForm()
+	if err != nil {
+		ctx.Error(fasthttp.StatusMessage(fasthttp.StatusInternalServerError), fasthttp.StatusInternalServerError)
+		return
+	}
+
+	// still require the old password before we can change it
+	currentPassword := form.Value["password"][0]
+	newPassword := form.Value["newPassword"][0]
+
+	if !server.CheckPasswordHash(currentPassword, user.Password) {
+		ctx.Error(fasthttp.StatusMessage(fasthttp.StatusForbidden), fasthttp.StatusForbidden)
+		return
+	}
+
+	newHashedPassword, err := server.HashPassword(newPassword)
+	if err != nil {
+		ctx.Error(fasthttp.StatusMessage(fasthttp.StatusInternalServerError), fasthttp.StatusInternalServerError)
+		return
+	}
+
+	user.Password = newHashedPassword
+	db.Save(&user)
+
+	ctx.Response.Header.SetStatusCode(fasthttp.StatusNoContent)
+	ctx.Write([]byte("Successfully changed your password!"))
 }
