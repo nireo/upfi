aa1fe0c2de5729a5c24127b0a3adb620e15db6d6
nireo <eemil.lehtikangas1@gmail.com>
2021-01-09 17:22:43 +0200

    removed a useless html file

--- json_api/create_router.go
+++ json_api/create_router.go
@@ -13,7 +13,11 @@ func CreateJSONRouter() *fasthttprouter.Router {
 
 	// Use the optimized api file upload function since we can't really upload files using json.
 	// So we just reuse the old since no need to copy-and-paste.
-	router.POST("/upload", middleware.CheckAuthentication(optimized_api.UploadFile))
+	router.POST("/api/upload", middleware.CheckAuthentication(optimized_api.UploadFile))
+
+	router.DELETE("/api/username", middleware.CheckAuthentication(optimized_api.DeleteUser))
+	router.PATCH("/api/password", middleware.CheckAuthentication(UpdatePassword))
+	router.PATCH("/api/settings", middleware.CheckAuthentication(HandleSettingsChange))
 
 	router.GET("/api/me", middleware.CheckAuthentication(WhoAmI))
 
