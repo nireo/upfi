5c22a55fa993051c2e51f0db1968e51e2fe11229
nireo <eemil.lehtikangas1@gmail.com>
2021-04-28 21:16:55 +0300

    Finished the files handler

--- web/files.go
+++ web/files.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"os"
 	"path/filepath"
 
 	"github.com/julienschmidt/httprouter"
@@ -180,6 +181,166 @@ func GetUserFiles(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	}
 
 	if err := templates.Files(w, pageParams); err != nil {
+		return
+	}
+}
+
+// UpdateFile is http handler which takes a file id as a query parameter and checks for the file's ownership.
+// This handler can be used to update file title and description.
+// Also the route is protected, so that the security token is checked before calling this handler.
+func UpdateFile(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	username := r.Header.Get("username")
+	db := lib.GetDatabase()
+
+	// Parse the multipart form so that we can take the 'title' and 'description' fields.
+	if err := r.ParseMultipartForm(1 << 20); err != nil {
+		ErrorPageHandler(w, r, lib.BadRequestErrorPage)
+		return
+	}
+
+	// Check that the input fields are included, because without this check there will be a
+	// index out of bounds error, if any of the fields are missing.
+	if len(r.Form["title"]) == 0 || len(r.Form["description"]) == 0 {
+		ErrorPageHandler(w, r, lib.BadRequestErrorPage)
+		return
+	}
+
+	title := r.Form["title"][0]
+	description := r.Form["description"][0]
+
+	// The description can be empty, but the title cannot
+	if title == "" {
+		ErrorPageHandler(w, r, lib.BadRequestErrorPage)
+		return
+	}
+
+	// Find the user that is requesting this handler.
+	user, err := models.FindOneUser(&models.User{Username: username})
+	if err != nil {
+		ErrorPageHandler(w, r, lib.NotFoundErrorPage)
+		return
+	}
+
+	// Find the file
+	fileID := ps.ByName("file")
+	var file models.File
+	if err := db.Where(&models.File{UUID: fileID}).First(&file).Error; err != nil {
+		ErrorPageHandler(w, r, lib.NotFoundErrorPage)
+		return
+	}
+
+	// Check that the user owns the file
+	if user.ID != file.UserID {
+		ErrorPageHandler(w, r, lib.NotFoundErrorPage)
+		return
+	}
+
+	// Update only the fields, which are not empty.
+	if description != "" {
+		file.Description = description
+	}
+
+	if title != "" {
+		file.Filename = title
+	}
+
+	// Save the changes to the database.
+	db.Save(&file)
+
+	http.Redirect(w, r, "/files", http.StatusMovedPermanently)
+}
+
+// DeleteFile is a handler that deletes a file owned by the user. The handler takes a file id as a query parameter
+// and then does checking on the ownership of the file.
+// Also the route is protected, so that the security token is checked before calling this handler.
+func DeleteFile(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	username := r.Header.Get("username")
+	db := lib.GetDatabase()
+
+	// Find the database entry of the user that requested this handler.
+	var user models.User
+	if err := db.Where(&models.User{Username: username}).First(&user).Error; err != nil {
+		ErrorPageHandler(w, r, lib.NotFoundErrorPage)
+		return
+	}
+
+	// Find the file, if the file does not exist, return a not found error
+	fileID := ps.ByName("file")
+	var file models.File
+	if err := db.Where(&models.File{UUID: fileID}).First(&file).Error; err != nil {
+		ErrorPageHandler(w, r, lib.NotFoundErrorPage)
+		return
+	}
+
+	// Check that the user owns the file.
+	if user.ID != file.UserID {
+		// Return a not found error, since we don't want the unauthorized user to know about the
+		// file's existence
+		ErrorPageHandler(w, r, lib.NotFoundErrorPage)
+		return
+	}
+
+	// Remove the file, if the file cannot be removed the return a internal server error to the user.
+	if err := os.Remove(lib.AddRootToPath("files/") + user.UUID + "/" + fmt.Sprintf("%s%s", file.UUID, file.Extension)); err != nil {
+		ErrorPageHandler(w, r, lib.InternalServerErrorPage)
+		return
+	}
+
+	// Delete the database entry
+	db.Delete(&file)
+	http.Redirect(w, r, "/files", http.StatusMovedPermanently)
+}
+
+// DownloadFile handler lets the user download a file. It also checks that the user owns the file he is trying download.
+// Also the route is protected, so that the security token is checked before calling this handler.
+func DownloadFile(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	username := r.Header.Get("username")
+	db := lib.GetDatabase()
+
+	var user models.User
+	if err := db.Where(&models.User{Username: username}).First(&user).Error; err != nil {
+		ErrorPageHandler(w, r, lib.NotFoundErrorPage)
+		return
+	}
+
+	fileID := ps.ByName("file")
+	var file models.File
+	if err := db.Where(&models.File{UUID: fileID}).First(&file).Error; err != nil {
+		ErrorPageHandler(w, r, lib.NotFoundErrorPage)
+		return
+	}
+
+	// Check that the user owns the file.
+	if user.ID != file.UserID {
+		ErrorPageHandler(w, r, lib.ForbiddenErrorPage)
+		return
+	}
+
+	if err := r.ParseMultipartForm(1 << 20); err != nil {
+		ErrorPageHandler(w, r, lib.BadRequestErrorPage)
+		return
+	}
+
+	if len(r.Form["master"]) == 0 {
+		ErrorPageHandler(w, r, lib.BadRequestErrorPage)
+		return
+	}
+
+	path := fmt.Sprintf("%s/%s/%s%s", lib.AddRootToPath("files"),
+		user.UUID, file.UUID, file.Extension)
+	w.Header().Set("Content-Type", file.MIME)
+
+	tempUUID := lib.GenerateUUID()
+	tempPath := fmt.Sprintf("%s/%s%s", lib.AddRootToPath("temp"),
+		tempUUID, file.Extension)
+	if err := crypt.DecryptToDst(tempPath, path, r.Form["master"][0]); err != nil {
+		ErrorPageHandler(w, r, lib.InternalServerErrorPage)
+		return
+	}
+
+	http.ServeFile(w, r, tempPath)
+	if err := os.Remove(tempPath); err != nil {
+		ErrorPageHandler(w, r, lib.InternalServerErrorPage)
 		return
 	}
 }
